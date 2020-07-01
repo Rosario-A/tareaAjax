0d16b1f16928388a8637fba80815f1ded5c0aa34
Rosario-A <ralegre@unsa.edu.pe>
2020-07-01 02:40:42 -0500

    Inicia y cierra sesión completado

--- DJangoDestinosTurísticos/src/accounts/views.py
+++ DJangoDestinosTurísticos/src/accounts/views.py
@@ -3,6 +3,24 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    
+    else:
+        return render(request,'login.html')
+
+
 def register(request):
 
     if request.method == 'POST':
@@ -24,6 +42,7 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save();
                 print('user created')
+                return redirect('login')
 
         else:
             messages.info(request, 'password not matching..') 
@@ -31,3 +50,7 @@ def register(request):
         return redirect('/')
     else:
         return render(request,'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
