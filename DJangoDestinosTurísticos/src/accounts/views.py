04846363a26a2a0424b2983552c354037bba5536
Rosario-A <ralegre@unsa.edu.pe>
2020-07-01 01:41:05 -0500

    Usuarios creados

--- DJangoDestinosTurísticos/src/accounts/views.py
+++ DJangoDestinosTurísticos/src/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 
@@ -12,9 +13,21 @@ def register(request):
         password2 = request.POST['password2']
         email = request.POST['email']
 
-        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-        user.save();
-        print('user created')
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save();
+                print('user created')
+
+        else:
+            messages.info(request, 'password not matching..') 
+            return redirect('register')
         return redirect('/')
     else:
         return render(request,'register.html')
